37809a027a7a604f44b4f467e23b5a496724ceff
silviadelya <silviadelyaheryani31@gmail.com>
2021-10-29 23:05:15 +0700

    Program

---
+++ menghitung-lingkaran.py
@@ -0,0 +1,8 @@
+print("Program Menghitung Luas dan Keliling Lingkaran")
+print()
+
+r = float(input("Jari-jari Lingkaran : "))
+luas = 3.14*(r*r)
+print("Luas Lingkaran = ",luas)
+keliling = 2*3.14*r
+print("Keliling Lingkaran = ",keliling)
